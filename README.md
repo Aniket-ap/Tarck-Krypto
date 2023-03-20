76f79fdc2aaf086b24e3fe1074a95d46a66480ca
Aniket Pradhan <aniketpradhan814@gmail.com>
2023-03-20 12:44:03 +0530

    Starter file added

---
+++ README.md
@@ -0,0 +1,1 @@
+# TRACK KRYPTO
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <h1>Tarck Crypto Currency</h1>
+    </div>
+  )
+}
+
+export default App
